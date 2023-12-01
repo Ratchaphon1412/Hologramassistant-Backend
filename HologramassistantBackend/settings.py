fe4426385614acb3ddfe48d175953b483858dd7d
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-01 10:33:25 +0700

    install drf generator command and setting django rest framework

--- HologramassistantBackend/settings.py
+++ HologramassistantBackend/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'drf_generators',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
