244c4cb97ad504b373ff676749d6c91f6a478443
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-04 02:17:01 +0700

    execption request to wit.ai and chenge authorization add bear token

--- utils/AI/Wit.py
+++ utils/AI/Wit.py
@@ -6,22 +6,29 @@ import json
 class Wit:
     def __init__(self, witAPI):
         self.witapiKey = witAPI
-        self.endpointMessage = "https://api.wit.ai/message?v=20230204&q="
+        self.endpointMessage = "https://api.wit.ai/message?v=20231202&q="
 
     def getIntent(self, text):
-        headers = {
-            'Authorization': self.witapiKey
-        }
-        response = requests.get(self.endpointMessage+text, headers=headers)
-        dicresponse = json.loads(response.text)
 
-        intent = None
-        confidence = None
-        entities = None
-        if (dicresponse["intents"] != list()):
-            intent = dicresponse["intents"][0]["name"]
-            confidence = dicresponse["intents"][0]["confidence"]
-        if (dicresponse["entities"] != dict()):
-            entities = dicresponse["entities"]
+        headers = {
+            'Authorization': 'Bearer ' + self.witapiKey
+        }
+        try:
+            response = requests.get(self.endpointMessage+text, headers=headers)
+            dicresponse = json.loads(response.text)
+
+            intent = None
+            confidence = None
+            entities = None
+            if (dicresponse["intents"] != list()):
+                intent = dicresponse["intents"][0]["name"]
+                confidence = dicresponse["intents"][0]["confidence"]
+            if (dicresponse["entities"] != dict()):
+                entities = dicresponse["entities"]
+            return intent, confidence, entities
+        except Exception as e:
+            intent = None
+            confidence = None
+            entities = None
 
         return intent, confidence, entities
