ed9c7d49884229a9da6be8c73b311834dd95fa17
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-04 18:11:56 +0700

    add url user app

--- HologramassistantBackend/urls.py
+++ HologramassistantBackend/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('api.urls')),
+    path('api/v1/', include('user.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
